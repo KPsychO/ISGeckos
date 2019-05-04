61cbe1a98402f95c998cc86770514b8d410d9e38
Alfon <alfonsotercer@gmail.com>
2019-05-04 14:12:35 +0200

    Revert "Botón publicación añadido"
    This reverts commit bea498de26eaad0cccac5d15d1f5922cd001dfbf.

--- initIS/src/Formulario/Control/FormularioDAO.java
+++ initIS/src/Formulario/Control/FormularioDAO.java
@@ -5,6 +5,8 @@ import org.json.JSONArray;
 public interface FormularioDAO {
 	
 	public abstract JSONArray getFormularies();
+	
+	//Por que esto era un boolean?
 	public abstract void insertFormulary(FormularioDTO newForm);
 	
 }
