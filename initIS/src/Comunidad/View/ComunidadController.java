88f6c98a40fba8bee4c53271806a51aab189f821
ignacico <50233310+ignacico@users.noreply.github.com>
2019-05-08 21:54:28 +0200

    Update ComunidadController.java

--- initIS/src/Comunidad/View/ComunidadController.java
+++ initIS/src/Comunidad/View/ComunidadController.java
@@ -5,17 +5,7 @@ import javax.swing.JPanel;
 import Usuario.Control.UsuarioDTO;
 import common.Controller;
 
-public class ComunidadController extends Controller {
+public class ComunidadController {
 
-	@Override
-	public JPanel getPanel(String panel, Object o, UsuarioDTO user) {
-		if (panel.equals("PerfilUsuarioDenunciado"))
-			return new MainViewPerfilUsuarioDenunciado((UsuarioDTO) o);
-		else if (panel.equals("Comunidad")) {
-			return new MainViewComunidad(user);
-		}
-		else
-			return null;
-	}
 
 }
