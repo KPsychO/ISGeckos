466318c297097964027f50d7e68e6af10209a460
ignacico <50233310+ignacico@users.noreply.github.com>
2019-05-09 23:49:49 +0200

    Add files via upload

--- initIS/src/Juego/Control/JuegoControllerFacade.java
+++ initIS/src/Juego/Control/JuegoControllerFacade.java
@@ -1,5 +1,7 @@
 package Juego.Control;
 
+import java.io.IOException;
+
 import javax.swing.JPanel;
 
 import Juego.View.MainViewActualizarJuego;
@@ -45,6 +47,10 @@ public class JuegoControllerFacade {
 	
 	public JPanel getDesarrolladoraPanel(UsuarioDTO dto) {
 		return new MainViewDeveloper(dto, this);
+	}
+	
+	public JPanel getListValoraciones(JuegoDTO game) throws IOException {
+		return this._controller.getListValoraciones(game);
 	}
 
 }
